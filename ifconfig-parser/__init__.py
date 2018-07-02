9596db2f7512975c8138deec1c3ce86820ea490b
l.siemiaszko <123qweasd>
2018-07-02 11:00:10 +0200

    Updated __init__.py

--- ifconfig-parser/__init__.py
+++ ifconfig-parser/__init__.py
@@ -9,3 +9,10 @@
 #    Python Version: 3.6
 #
 # ======================================================
+from ifconfig_parser import IfconfigParser
+
+__title__ = "ifconfig_parser"
+__version__ = '0.1.0'
+__author__ = 'ThreeHeadedKnight'
+
+__all__ = ['IfconfigParser']
