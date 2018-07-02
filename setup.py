80147712aba883899e65e2f65ebbf42e24590cbd
l.siemiaszko <123qweasd>
2018-07-02 12:07:04 +0200

    Updated Travis CI job file

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# ======================================================
+#
+#    File name: setup.py
+#    Author: threeheadedknight@protonmail.com
+#    Date created: 02.07.2018 11:33
+#    Python Version: 3.6
+#
+# ======================================================
+from setuptools import setup
+
+
+setup(
+    name='ifconfig-parser',
+    version='0.1.0',
+    url='',
+    download_url='',
+    license='MIT',
+    description='',
+    long_description='',
+    author='ThreeHeadedKnight',
+    author_email='threeheadedknight@protonmail.com',
+    packages='',
+    package_data='',
+)
