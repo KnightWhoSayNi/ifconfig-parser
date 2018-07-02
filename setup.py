a2ad235bd7befb4c5a197f434c6b663e2bc51d5c
l.siemiaszko <123qweasd>
2018-07-02 14:27:03 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,13 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-# ======================================================
-#
-#    File name: setup.py
-#    Author: threeheadedknight@protonmail.com
-#    Date created: 02.07.2018 11:33
-#    Python Version: 3.6
-#
-# ======================================================
 from setuptools import setup
 
 
 setup(
     name='ifconfig-parser',
-    version='0.1.0',
-    url='',
-    download_url='',
-    license='MIT',
-    description='',
-    long_description='',
-    author='ThreeHeadedKnight',
+    url='https://github.com/KnightWhoSayNi/ifconfig-parser',
+    author='KnightWhoSayNi',
     author_email='threeheadedknight@protonmail.com',
-    packages='',
-    package_data='',
+    packages=['ifconfig-parser'],
+    version='0.1',
+    license='MIT',
+    description='ifconfig-parser',
 )
