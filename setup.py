14709a000a4129db1cc681ac3c57322840eb0636
l.siemiaszko <123qweasd>
2018-07-02 15:33:52 +0200

    Cleanup repo

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='ifconfigparser',
+    name='ifconfig-parser',
+    version='0.0.4',
     url='https://github.com/KnightWhoSayNi/ifconfig-parser',
     author='KnightWhoSayNi',
     author_email='threeheadedknight@protonmail.com',
-    packages=['ifconfig-parser'],
-    version='0.0.3',
-    license='MIT',
+    packages=['ifconfigparser'],
+    package_dir={'ifconfigparser': 'ifconfigparser'},
+    license='MIT license',
     description='ifconfig-parser',
     classifiers=(
         "Programming Language :: Python :: 3",
