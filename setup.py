0840ba130c8751700770248d51c6faa3dc567e38
l.siemiaszko <123qweasd>
2018-07-02 14:35:07 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
-from setuptools import setup
-
+from setuptools import setup, find_packages
 
 setup(
     name='ifconfig-parser',
     url='https://github.com/KnightWhoSayNi/ifconfig-parser',
     author='KnightWhoSayNi',
     author_email='threeheadedknight@protonmail.com',
-    packages=['ifconfig-parser'],
-    version='0.1',
+    packages=find_packages(),
+    version='0.0.1',
     license='MIT',
     description='ifconfig-parser',
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
 )
