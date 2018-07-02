66ca494ff5aae6bcab6cc9c16a6894e4a7a02785
l.siemiaszko <123qweasd>
2018-07-02 16:05:02 +0200

    Update __init__.py

--- ifconfigparser/tests/__init__.py
+++ ifconfigparser/tests/__init__.py
@@ -3,15 +3,9 @@
 
 # ======================================================
 #
-#    File name: __init__.py
+#    File name: __init__
 #    Author: threeheadedknight@protonmail.com
-#    Date created: 30.06.2018 17:00
+#    Date created: 02.07.2018 16:03
 #    Python Version: 3.6
 #
 # ======================================================
-from .ifconfig_parser import IfconfigParser
-
-
-__author__ = "KnightWhoSayNi"
-__email__ = 'threeheadedknight@protonmail.com'
-__version__ = '0.0.1b'
